741d33224a67f8c5838f029c0b073b5a0745c632
Arman Siddiqui <arman2113022@akgec.ac.in>
2023-12-02 20:20:14 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// const fs = require("fs");
+// import generate from "sillyname";
+import gen from "superheroes";
+// const silly = require("sillyname");
+let s = gen.random();
+console.log(s);
+
+// fs.writeFile("message.txt", "fuck you",(err)=>{
+//     if(err) throw err;
+//     console.log("yeah");
+// });
+// fs.readFile("./message.txt",'utf8',(err,data)=>{
+// if(err) throw err;
+// console.log(data);
+// })
